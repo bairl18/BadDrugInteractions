d41b250310250908f40c45bf4f72a35f801fc95e
watanabe18 <watanabe18@up.edu>
2017-11-08 17:18:45 -0800

    Revert "added search functionality an GUI integration"
    This reverts commit 6538798d91c8290299c1eee40154ad0f7af9e9b6.

--- app/src/main/java/com/example/linnea/baddruginteractions2/SearchActivity.java
+++ app/src/main/java/com/example/linnea/baddruginteractions2/SearchActivity.java
@@ -9,38 +9,33 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.List;
-
-import static com.example.linnea.baddruginteractions2.R.id.search;
 import static java.sql.Types.NULL;
 
 public class SearchActivity extends AppCompatActivity {
 
     DBHandler db = new DBHandler(this);
+    UserProfileHandler up = new UserProfileHandler(this);
+    UserDrug selectedDrug;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        final ListView fdaMedsList = (ListView) findViewById(R.id.fdaMedsList);
-        final Button search = (Button) findViewById(R.id.search);
         final TextView searchField = (TextView) findViewById(R.id.searchField);
-        search.setOnClickListener(new View.OnClickListener()
+        searchField.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                String searchText = "";
-                if(searchField.getText() != null) {
-                    searchText = searchField.getText().toString();
-                }
+                String search = searchField.getText().toString();
 
-                if (searchText.equals(NULL))
+                if (search.equals(NULL))
                 {
                     //toast that says enter a valid medication
                     String message = "Please enter a valid medication.";
@@ -48,49 +43,56 @@ public class SearchActivity extends AppCompatActivity {
                 }
                 else
                 {
-                    //List<Drug> drugList = db.getDrugList(searchText);
-                    //if(drugList.isEmpty()){
-                    //}
-                    //else {
-                    //searchField.setText("" + drugList.get(0).getDrug_name());
-                    populateMedsList(searchText);
-                    //}
+                   //query
                 }
             }
         });
 
+        populateMedsList();
         registerClickCallback();
+
+        ImageButton add = (ImageButton)findViewById(R.id.addButton);
+        add.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+                if(selectedDrug != null)
+                {
+                    up.addDrug(selectedDrug);
+                }
+
+            }
+        });
+
     }
 
 
 
-    private void populateMedsList(String searchText)
+    private void populateMedsList()
     {
-        List<Drug> drugList = db.getDrugList(searchText);
         // Create list of meds
-        if(!drugList.isEmpty()) {
-            String[] drugNames = new String[drugList.size()];
-            for (int i = 0; i < drugList.size(); i++) {
-                drugNames[i] = drugList.get(i).getDrug_name();
-            }
+        Drug[] fdaMeds = {};
 
-            // Build adapter
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_items_layout, drugNames);
+        // Build adapter
+        ArrayAdapter<Drug> adapter = new ArrayAdapter<Drug>(this, R.layout.list_items_layout, fdaMeds);
 
-            // Configure list view
-            ListView fdaMedsList = (ListView) findViewById(R.id.fdaMedsList);
-            fdaMedsList.setAdapter(adapter);
-        }
+        // Configure list view
+        ListView fdaMedsList = (ListView) findViewById(R.id.fdaMedsList);
+        fdaMedsList.setAdapter(adapter);
     }
 
     private void registerClickCallback()
     {
-        ListView fdaMedsList = (ListView) findViewById(R.id.fdaMedsList);
+        final ListView fdaMedsList = (ListView) findViewById(R.id.fdaMedsList);
         fdaMedsList.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
             @Override
             public void onItemClick(AdapterView<?> paret, View viewClicked, int position, long id)
             {
+                 selectedDrug = (UserDrug)fdaMedsList.getItemAtPosition(position);
+
                 TextView textView = (TextView) viewClicked;
                 String message = "You clicked # " + position + ", which is string: " + textView.getText().toString();
                 Toast.makeText(SearchActivity.this, message, Toast.LENGTH_LONG).show();
