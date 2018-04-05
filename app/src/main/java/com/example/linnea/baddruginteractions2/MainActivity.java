df77a150b7bc804519bbc89a6131ffd527f5ab23
bairl18 <bairl18@up.edu>
2018-04-04 18:09:44 -0700

    Interactions listed on main activity; reset button on settings activity

--- app/src/main/java/com/example/linnea/baddruginteractions2/MainActivity.java
+++ app/src/main/java/com/example/linnea/baddruginteractions2/MainActivity.java
@@ -1,27 +1,18 @@
 package com.example.linnea.baddruginteractions2;
 
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.AssetManager;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.widget.*;
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-
-
 import android.util.Log;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -61,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         search.setOnClickListener(this);
         settings.setOnClickListener(this);
         reminders.setOnClickListener(this);
+        logo.setOnClickListener(this);
 
         // Create databases
         Log.d("MainActivity", "Opening User medication DB");
@@ -102,6 +94,94 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 intent = new Intent(MainActivity.this, RemindersActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.logo:
+                // Create pop-up with all interactions
+                final List<Interaction> ints = interactions.asList();
+                final String[] intStrings = new String[ints.size()];
+
+                for (int i = 0; i < ints.size(); i++)
+                {
+                    Interaction currentInt = ints.get(i);
+                    String drug1 = currentInt.getDrugName1();
+                    String drug2 = currentInt.getDrugName2();
+                    String sev = currentInt.getSeverity();
+
+                    String intString = drug1 + " and " + drug2;
+                    intStrings[i] = intString;
+                }
+
+                //ListView intsListView;
+                //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, intsListView, intStrings);
+
+                boolean empty = true;
+                for (int i=0; i< intStrings.length; i++) {
+                    if (intStrings[i] != null) {
+                        empty = false;
+                        break;
+                    }
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                TextView title = new TextView(getApplicationContext());
+
+                if(empty)
+                {
+                    title.setText("You have no drug interactions!");
+                }
+                else
+                {
+                    title.setText("Your Drug Interactions");
+                    builder.setItems(intStrings, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int item) {
+                            // Do something with the selection
+                            Interaction i = ints.get(item);
+                            String description = i.getDescription();
+                            String severity = i.getSeverity();
+
+                            AlertDialog alertDialog2 = new AlertDialog.Builder(MainActivity.this).create();
+
+                            TextView dialogTitle2 = new TextView(getApplicationContext());
+                            dialogTitle2.setText(severity);
+                            dialogTitle2.setTextColor(Color.parseColor("#c70039"));
+                            dialogTitle2.setTypeface(dialogTitle2.getTypeface(), Typeface.BOLD);
+                            dialogTitle2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+                            dialogTitle2.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                            alertDialog2.setCustomTitle(dialogTitle2);
+
+                            alertDialog2.setMessage(description);
+
+                            alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "OK" ,
+                                    new DialogInterface.OnClickListener()
+                                    {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which)
+                                        {
+                                            dialog.dismiss();
+                                            //alertDialog.show();
+                                        }
+                                    });
+
+                            alertDialog2.show();
+                            TextView dialogTextView2 = (TextView)alertDialog2.findViewById(android.R.id.message);
+                            dialogTextView2.setTextSize(20);
+                        }
+                    });
+                }
+                title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
+                title.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                builder.setCustomTitle(title);
+
+                AlertDialog alert = builder.create();
+
+                if (!empty)
+                {
+                    ListView alertListView = alert.getListView();
+                    alertListView.setDivider(new ColorDrawable(Color.LTGRAY));
+                    alertListView.setDividerHeight(2);
+                }
+
+                alert.show();
+
             default:
                 // Do nothing
         }
--- app/src/main/java/com/example/linnea/baddruginteractions2/SettingsActivity.java
+++ app/src/main/java/com/example/linnea/baddruginteractions2/SettingsActivity.java
@@ -2,12 +2,33 @@ package com.example.linnea.baddruginteractions2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        reset = (Button)findViewById(R.id.reset);
+        reset.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        //String viewStr = view.toString();
+        switch(view.getId())
+        {
+            case R.id.reset:
+                // Reset all
+
+            default:
+                // Do nothing
+        }
     }
 }
